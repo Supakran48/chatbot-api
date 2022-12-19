f18e61f7200aaf1da2e3b383ad6d13dd4e5849c5
Supakran48 <flukeark1493@gmail.com>
2022-12-19 15:56:07 +0700

    update

---
+++ services/handle-event.js
@@ -0,0 +1,29 @@
+const service = require('./handle-message')
+
+// event handler
+exports.handleEvent = (event) => {
+    switch (event.type) {
+        case "message":
+            switch (event.message.type) {
+                case "text":
+                    // console.log("text message")
+                    service.handleMessage(event)
+                    break;
+                case "sticker":
+                    console.log("sticker message")
+                    break;
+                case "image":
+                    // console.log("images message")
+                    console.log(event.message)
+                    break;
+                default:
+                    throw new Error("Unknow message" + JSON.stringify(event.message.type))
+            }
+            break;
+        case "postback":
+            console.log("postback")
+            break;
+        default:
+            throw new Error("Unknow event" + JSON.stringify(event))
+    }
+}
